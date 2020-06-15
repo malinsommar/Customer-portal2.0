9fa419518d556bd87eb3a910e5cbc839bd6d81d0
95jassod <jasminesoderberg@hotmail.se>
2020-06-15 12:14:25 +0200

    Home page profile, and css hone

--- customer-portal/src/pages/Home.js
+++ customer-portal/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 import HeaderComponent from "../components/Header";
 import ButtonComponent from "../components/Button";
+import CardComponent from "../components/CardComponent";
 import "../style/home.css";
 import "../style/buttons.css";
 
@@ -16,6 +18,38 @@ const Home = () => {
             <strong> "Datum".</strong> Ta gärna en titt!
           </p>
         </div>
+        <div id="MyProfileHome">
+          <CardComponent children={MyProfileHome()} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const MyProfileHome = () => {
+  return (
+    <div>
+      <p id="myProfile">Min Profil</p>
+      <NavLink
+        className="navLink"
+        to="/settings/myProfile"
+        activeClassName="isActive"
+      >
+        <ButtonComponent title="Redigera" className="homePageButtons" />
+      </NavLink>
+      <hr id="profile"></hr>
+
+      <p>Bild</p>
+
+      <p className="info">Föredragna industrier</p>
+
+      <p className="info">Kontaktuppgifter</p>
+      <div>
+        <p className="info-profile">Telefon: {"phonenumber"}</p>
+        <p className="info-profile">Mail: {"email"}</p>
+        <p className="info-profile">Addres: {"adress"}</p>
+        <p className="info-profile">Postnummer: {"zip-code"}</p>
+        <p className="info-profile">Postort: {"city"}</p>
       </div>
     </div>
   );
